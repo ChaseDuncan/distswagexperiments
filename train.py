ded159006d2597589682037e6599632154a8bdde
Chase Duncan <cddunca2@frankenstein.bioen.illinois.edu>
2020-04-14 14:01:20 -0500

    Baseline done. Faux-distributed training done.'

---
+++ train.py
@@ -0,0 +1,96 @@
+# python modules
+import os
+
+# random modules
+from tqdm import tqdm
+
+# pytorch modules
+import torch
+import torchvision.models as models
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import LambdaLR
+
+import utils
+# Directory to retrieve or store CIFAR10 dataset. This directory will
+# be created if it doesn't exist already.
+model_name = 'baseline'
+ckpt_dir = 'checkpoints/'+ model_name + '/'
+cifar10_path = '/shared/mrfil-data/cddunca2/cifar10/'
+epochs = 150
+swa_start = 125
+swa_lr = 0.05
+lr_init = 0.1
+momentum = 0.0
+
+verbose = True
+
+lr_decay = lr_init / epochs
+
+device = torch.device('cuda')
+
+# Create data directories as necessary.
+if not os.path.exists(ckpt_dir):
+  print('[INFO] Make dir %s' % ckpt_dir)
+  os.makedirs(ckpt_dir)
+
+if not os.path.exists(cifar10_path):
+  print('[INFO] Make dir %s' % cifar10_path)
+  os.makedirs(cifar10_path)
+
+# Taken from swag experiments
+# https://github.com/wjmaddox/drbayes/blob/master/subspace_inference/models/vgg.py
+transform_train = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.Resize(32),
+    transforms.RandomCrop(32, padding=4),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+])
+
+cifar10_train_dataset = datasets.CIFAR10(cifar10_path, 
+                      train=True, download=True, transform=transform_train)
+
+cifar10_train_loader = DataLoader(cifar10_train_dataset, shuffle=True, batch_size=128)
+
+model = models.vgg16()
+model = model.to(device)
+
+optimizer = torch.optim.SGD(model.parameters(), lr=lr_init, 
+    momentum=momentum, weight_decay=1e-5)
+
+# time-based lr decay
+#lr_lambda = lambda epoch: (1. / lr_decay*epoch)
+
+# SWA lr dacay
+lr_lambda = lambda epoch: utils.schedule(epoch, swa_start, swa_lr, lr_init)
+# learning rate scheduler
+scheduler = LambdaLR(optimizer, lr_lambda=lr_lambda)
+# L2 regularized cross entropy loss
+loss = torch.nn.CrossEntropyLoss()
+
+sample = False
+for epoch in range(epochs):
+  total_loss = 0
+  model.train()
+
+  if epoch >= swa_start:
+    sample = True 
+
+  if verbose:
+    cifar10_train_loader = tqdm(cifar10_train_loader)
+
+  for src, target in cifar10_train_loader:
+    optimizer.zero_grad()
+    src, target = src.to(device), target.to(device)
+    output = model(src)
+    cur_loss = loss(output, target)
+    total_loss += cur_loss
+    cur_loss.backward()
+    optimizer.step()
+  if sample:
+    utils.save_checkpoint(ckpt_dir, epoch=epoch, name=model_name+f'{epoch:03}',
+                          state_dict=model.state_dict(), optimizer=optimizer.state_dict())
+    
---
+++ utils.py
@@ -0,0 +1,79 @@
+import os
+import torch
+
+def moving_average(net1, net2, alpha=1):
+    for param1, param2 in zip(net1.parameters(), net2.parameters()):
+        param1.data *= (1.0 - alpha)
+        param1.data += param2.data * alpha
+
+def swa_moment1(moment1, obs, n):
+    for param1, param2 in zip(moment1.parameters(), obs.parameters()):
+        param1.data *= n
+        param1.data += param2.data 
+        param1.data /= 1. / (n + 1)
+
+def swa_moment2(moment2, net2, n):
+    for param1, param2 in zip(moment2.parameters(), net2.parameters()):
+        param1.data *= n
+        param1.data += param2.data * param2.data
+        param1.data /= 1. / (n + 1)
+
+def save_checkpoint(dir, epoch=None, name='checkpoint', **kwargs):
+    state = {
+        'epoch': epoch,
+    }
+    if epoch is not None:
+       name = '%s-%d.pt' % (name, epoch)
+    else:
+       name = '%s.pt' % (name)
+    state.update(kwargs)
+    filepath = os.path.join(dir, name)
+    torch.save(state, filepath)
+
+def adjust_learning_rate(optimizer, lr):
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+    return lr
+
+# args.swa_start <- the epoch to begin swa sampling of parameters
+# args.swa_lr <- the constant learning rate at which the swa sampling is performed.
+# args.lr_init <- the learning rate for the constant lr period
+def schedule(epoch, swa_start, swa_lr, lr_init):
+    t = epoch / swa_start
+    lr_ratio = swa_lr / lr_init
+    # TODO: replace this with something more sophisticated?
+    if t <= 0.5:
+        factor = 1.0
+    elif t <= 0.9:
+        factor = 1.0 - (1.0 - lr_ratio) * (t - 0.5) / 0.4
+    else:
+        factor = lr_ratio
+    return lr_init * factor
+
+#def train_epoch(loader, model, criterion, optimizer):
+#    loss_sum = 0.0
+#    correct = 0.0
+#
+#    model.train()
+#
+#    for i, (input, target) in enumerate(loader):
+#        input = input.cuda(async=True)
+#        target = target.cuda(async=True)
+#        input_var = torch.autograd.Variable(input)
+#        target_var = torch.autograd.Variable(target)
+#
+#        output = model(input_var)
+#        loss = criterion(output, target_var)
+#
+#        optimizer.zero_grad()
+#        loss.backward()
+#        optimizer.step()
+#
+#        loss_sum += loss.item() * input.size(0)
+#        pred = output.data.max(1, keepdim=True)[1]
+#        correct += pred.eq(target_var.data.view_as(pred)).sum().item()
+#
+#    return {
+#        'loss': loss_sum / len(loader.dataset),
+#        'accuracy': correct / len(loader.dataset) * 100.0,
+#    }

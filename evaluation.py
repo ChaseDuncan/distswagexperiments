da1c017f668a85c991867475a0f1fae41ff1d952
Chase Duncan <cddunca2@frankenstein.bioen.illinois.edu>
2020-04-28 16:30:33 -0500

    Adding SWAG codebase to ours.

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,6 @@
 import os
+import argparse
+
 import torch
 import torchvision.models as models
 import torchvision.datasets as datasets
@@ -17,6 +19,7 @@ device = torch.device(f'cuda:{str(get_free_gpu())}')
 parser = argparse.ArgumentParser(description='D-SWAG experiemnts training script.') 
 parser.add_argument('--dir', type=str, default=None, required=True, help='training directory (default: None)')
 parser.add_argument('--data_path', type=str, default='/shared/mrfil-data/cddunca2/cifar10/', metavar='PATH',
+                    help='path to datasets location (default: /shared/mrfil-data/cddunca2/cifar10/)')
 args = parser.parse_args()
 
 N = 30
@@ -24,9 +27,7 @@ num_classes = 10
 var_clamp = 1e-6 
 eps = 1e-16 
 checkpoints = []
-num_checkpoints = None
-#num_checkpoints = 5
-save_path = f'{args}/evaluation/'
+save_path = f'{args.dir}evaluation'
 
 
 transform_test = transforms.Compose(
@@ -37,16 +38,19 @@ transform_test = transforms.Compose(
         ]
     )
 
-cifar10_test_dataset = datasets.CIFAR10(cifar10_path, 
+cifar10_test_dataset = datasets.CIFAR10(args.data_path, 
                       train=False, download=True, transform=transform_test)
 
 cifar10_test_loader = DataLoader(cifar10_test_dataset, shuffle=False, batch_size=128)
 
 
 # Gather model checkpoints
-for root, dirs, filenames in os.walk(f'/shared/mrfil-data/cddunca2/pgmproject/{model_name}'):
-  for name in filenames:
-    checkpoints.extend([os.path.join(root, name)])
+for root, dirs, filenames in os.walk(f'{args.dir}/checkpoints/'):
+  if filenames:
+    worker = []
+    for name in filenames:
+      worker.extend([os.path.join(root, name)])
+    checkpoints.append(worker)
 
 assert len(checkpoints) > 0
 
@@ -55,43 +59,53 @@ mean = torch.zeros(sum(param.numel() for param in model.parameters()))
 sq_mean = mean.clone() 
 
 # compute moments
-for n, checkpoint in enumerate(checkpoints):
-  print(f'Loading: {n} of {len(checkpoints)}')
-  chkpt = torch.load(checkpoint)
-  model.load_state_dict(chkpt['state_dict'])
-  mean, sq_mean = collect_model(model, mean, sq_mean, n)
+posterior_by_epoch = []
+for n, checkpoint in enumerate(zip(*checkpoints)):
+  print(f'Loading: {n} of {len(checkpoints[0])}')
+  chkpts = [torch.load(ckpt) for ckpt in checkpoint]
+  for chkpt in chkpts:
+    model.load_state_dict(chkpt['state_dict'])
+    mean, sq_mean = collect_model(model, mean, sq_mean, n)
+  posterior_by_epoch.append((mean, sq_mean))
 
-predictions = np.zeros((len(cifar10_test_loader.dataset), num_classes))
-targets = np.zeros(len(cifar10_test_loader.dataset))
 
 accuracy_per_epoch = []
-for i in range(N):
-  print("%d/%d" % (i + 1, N))
-  sample = sample_post(mean, sq_mean)  
-  set_weights(model, sample, device) 
-  
-  k = 0 
-  # Have to move data to device after setting the weights
-  model.to(device)
-  model.eval()
-  for input, target in tqdm(cifar10_test_loader):
-    input = input.to(device)
-    output = model(input)
-    with torch.no_grad():
-      predictions[k : k + input.size()[0]] += (
-        F.softmax(output, dim=1).cpu().numpy()
-      )
-    targets[k : (k + target.size(0))] = target.numpy()
-    k += input.size()[0]    
-  
-  accuracy =  np.mean(np.argmax(predictions, axis=1) == targets)
+nll_per_epoch = []
+for ep, (mean, sq_mean) in enumerate(posterior_by_epoch):
+  predictions = np.zeros((len(cifar10_test_loader.dataset), num_classes))
+  targets = np.zeros(len(cifar10_test_loader.dataset))
+  for i in range(N):
+    print("Epoch: %d, %d/%d" % (ep, i + 1, N))
+    sample = sample_post(mean, sq_mean)  
+    set_weights(model, sample, device) 
+    
+    k = 0 
+    # Have to move data to device after setting the weights
+    model.to(device)
+    model.eval()
+    for input, target in tqdm(cifar10_test_loader):
+      input = input.to(device)
+      output = model(input)
+      with torch.no_grad():
+        predictions[k : k + input.size()[0]] += (
+          F.softmax(output, dim=1).cpu().numpy()
+        )
+      targets[k : (k + target.size(0))] = target.numpy()
+      k += input.size()[0]    
+   
+  accuracy =  np.mean(np.argmax(predictions, axis=1)  == targets)
   accuracy_per_epoch.append(accuracy)
   print("Accuracy:", accuracy)
   #nll is sum over entire dataset
-  print("NLL:", nll(predictions / (i + 1), targets))
+  nll_eval = nll(predictions / N, targets)
+  print("NLL:", nll_eval)
+  nll_per_epoch.append(nll_eval)
+  print(accuracy_per_epoch)
+  print(nll_per_epoch)
 
 predictions /= N
 accuracy_per_epoch = np.array(accuracy_per_epoch)
 entropies = -np.sum(np.log(predictions + eps) * predictions, axis=1)
 np.savez(save_path, entropies=entropies, predictions=predictions, 
-    targets=targets, accuracy_per_epoch=accuracy_per_epoch)
+    targets=targets, accuracy_per_epoch=accuracy_per_epoch,
+    nll_per_epoch=nll_per_epoch)

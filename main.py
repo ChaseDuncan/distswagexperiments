a09bbfa474a6af9d346d29040e9009b2d12fe07f
Chase Duncan <cddunca2@frankenstein.bioen.illinois.edu>
2020-05-10 10:25:27 -0500

    - moved irrelevant command line args to a function in utils - script now puts stats, models, and all nondefault parameters to   the save directory

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 import json
 import time
 import os
+import sys
 
 import torch
 from torchvision import datasets, transforms
@@ -15,7 +16,7 @@ from src.utils import global_aggregate, network_parameters, test_inference
 from src.local_train import LocalUpdate
 from src.attacks import attack_updates
 from src.defense import defend_updates
-from src.utils import predict,bn_update
+from src.utils import predict,bn_update, pop_irrelevant_args
 from collections import OrderedDict, Counter
 import torchvision.models as models
 
@@ -25,53 +26,26 @@ warnings.filterwarnings("ignore")
 ############################## Reading Arguments ##############################
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dir', type=str, default='./outputs', help="directory for savingouputs")
-parser.add_argument('--fname', type=str, default='test.npz', help="directory for savingouputs")
 parser.add_argument('--exp_name', type=str, default='', help="name of the experiment")
-parser.add_argument('--seed', type=int, default=0, help="seed for running the experiments")
-parser.add_argument('--data_source', type=str, default="CIFAR10", help="dataset to be used", choices=['MNIST', 'CIFAR10'])
-parser.add_argument('--sampling', type=str, default="iid", help="sampling technique for client data", choices=['iid', 'non_iid'])
 parser.add_argument('--num_users', type=int, default=100, help="number of clients to create")
-parser.add_argument('--num_shards_user', type=int, default=2, help="number of classes to give to the user")
-parser.add_argument('--train_test_split', type=float, default=1.0, help="train test split at the client end")
-parser.add_argument('--train_batch_size', type=int, default=32, help="batch size for client training")
-parser.add_argument('--test_batch_size', type=int, default=32, help="batch size for testing data")
-
+parser.add_argument('--dir', type=str, default='./outputs', help="directory for savingoutputs")
+parser.add_argument('--fname', type=str, default='test.npz', help="directory for savingoutputs")
 parser.add_argument('--model', type=str, default="VGG16", help="network structure to be used for training", choices=['LR', 'MLP', 'CNN', 'VGG16'])
+parser.add_argument('--data_source', type=str, default="CIFAR10", help="dataset to be used", choices=['MNIST', 'CIFAR10'])
 parser.add_argument('--criterion', type=str, default="crossentropy", help="evaluation criterion", choices=['crossentrooy', 'NLL'])
-parser.add_argument('--device', type=str, default="gpu", help="device for Torch", choices=['cpu', 'gpu'])
 parser.add_argument('--frac_clients', type=float, default=0.1, help="proportion of clients to use for local updates")
 parser.add_argument('--global_optimizer', type=str, default='fedavg', help="global optimizer to be used", choices=['fedavg', 'fedavgm', 'scaffold', 'fedadam', 'fedyogi'])
 parser.add_argument('--global_epochs', type=int, default=100, help="number of global federated rounds")
 parser.add_argument('--global_lr', type=float, default=1, help="learning rate for global steps")
-parser.add_argument('--local_optimizer', type=str, default='sgd', help="local optimizer to be used", choices=['sgd', 'adam', 'pgd', 'scaffold'])
+parser.add_argument('--local_optimizer', type=str, default='sgd', help="local optimizer to be used", choices=['sgd', 'adam', 'pgd'])
 parser.add_argument('--local_epochs', type=int, default=20, help="number of local client training steps")
 parser.add_argument('--local_lr', type=float, default=1e-4, help="learning rate for local updates")
-parser.add_argument('--momentum', type=float, default=0.5, help="momentum value for SGD")
-parser.add_argument('--mu', type=float, default=0.1, help="proximal coefficient for FedProx")
-parser.add_argument('--beta1', type=float, default=0.9, help="parameter for FedAvgM and FedAdam")
-parser.add_argument('--beta2', type=float, default=0.999, help="parameter for FedAdam")
-parser.add_argument('--eps', type=float, default=1e-4, help="epsilon for adaptive methods")
-parser.add_argument('--frac_byz_clients', type=float, default=0.0, help="proportion of clients that are picked in a round")
-parser.add_argument('--is_attack', type=int, default=0, help="whether to attack or not")
-parser.add_argument('--attack_type', type=str, default='label_flip', help="attack to be used", choices=['fall', 'label_flip', 'little', 'gaussian'])
-parser.add_argument('--fall_eps', type=float, default=-5.0, help="epsilon value to be used for the Fall Attack")
-parser.add_argument('--little_std', type=float, default=1.5, help="standard deviation to be used for the Little Attack")
-parser.add_argument('--is_defense', type=int, default=0, help="whether to defend or not")
-parser.add_argument('--defense_type', type=str, default='median', help="aggregation to be used", choices=['median', 'krum', 'trimmed_mean'])
-parser.add_argument('--trim_ratio', type=float, default=0.1, help="proportion of updates to trim for trimmed mean")
-parser.add_argument('--multi_krum', type=int, default=5, help="number of clients to pick after krumming")
-parser.add_argument('--local_epochs_sampling', type=int, default=20, help="Number of local epochs without global aggregation (Phase2)")
-parser.add_argument('--rank_param', type=int, default=4, help="Low rank approxmation parameter")
-parser.add_argument('--num_samples', type=int, default=20, help="Number of samples in testing phase")
-parser.add_argument('--frac_users_phase2', type=float, default=1.0, help="Number of clients to run phase 2")
-
-
 parser.add_argument('--batch_print_frequency', type=int, default=100, help="frequency after which batch results need to be printed to the console")
 parser.add_argument('--global_print_frequency', type=int, default=1, help="frequency after which global results need to be printed to the console")
 parser.add_argument('--global_store_frequency', type=int, default=100, help="frequency after which global results should be written to CSV")
 parser.add_argument('--threshold_test_metric', type=float, default=0.9, help="threshold after which the code should end")
 
+pop_irrelevant_args(parser)
 obj = parser.parse_args()
 
 with open('config.json') as f:
@@ -80,6 +54,14 @@ with open('config.json') as f:
 obj = vars(obj)
 obj.update(json_vars)
 print(obj)
+
+if not(os.path.exists(obj["dir"])):
+    os.makedirs(obj["dir"], exist_ok=True)
+
+with open(os.path.join(obj["dir"], obj["fname"]), "w") as f:
+    f.write(" ".join(sys.argv))
+    f.write("\n")
+		
 K=obj['rank_param']
 
 np.random.seed(obj['seed'])
@@ -89,6 +71,7 @@ if obj['criterion'] == "NLL":
   criterion = torch.nn.NLLLoss() # Default criterion set to NLL loss function
 elif obj['criterion'] == "crossentropy":    
   criterion = torch.nn.CrossEntropyLoss()
+
 ############################### Loading Dataset ###############################
 if obj['data_source'] == 'MNIST':
   data_dir = 'data/'
@@ -361,10 +344,8 @@ swag_nlls = -np.mean(np.log(predictions[np.arange(predictions.shape[0]), targets
 print('Final Accurcay is',swag_accuracies)
 print('Final Negative log likelihood is',swag_nlls)
 
-if not(os.path.exists(obj["dir"])):
-    os.mkdir(obj["dir"])
-torch.save(mean,'results/%s_mean_model.pt'%(obj['exp_name']))
-torch.save(std,'results/%s_std_model.pt'%(obj['exp_name']))
+torch.save(mean, os.path.join(obj["dir"], f'{obj["exp_name"]}_mean_model.pt'))
+torch.save(std, os.path.join(obj["dir"], f'{obj["exp_name"]}_std_model.pt'))
 
 np.savez(
 		os.path.join(obj["dir"], obj["fname"]),

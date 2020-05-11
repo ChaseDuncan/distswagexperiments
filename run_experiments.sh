bbef996f019354bac582721d792a5c98d1c1909a
Chase Duncan <cddunca2@frankenstein.bioen.illinois.edu>
2020-05-10 23:24:14 -0500

    Changed order of variables that are being iterated over.

--- run_experiments.sh
+++ run_experiments.sh
@@ -13,8 +13,9 @@ global_epochs=150
 
 num_users=1
 num_samples=1
-for num_users in 1 2 4 10;do
-  for num_samples in 1 5 10 20;do
+
+for num_samples in 1 5 10 20;do
+  for num_users in 1 2 4 10;do
     exp_name="VC-${num_users}-${global_epochs}-${local_epochs}-${local_epochs_sampling}-${num_samples}"
     python main.py --exp_name=${exp_name} --num_users=${num_users} --global_epochs=${global_epochs} \
       --local_epochs=${local_epochs} --local_epochs_sampling=${local_epochs_sampling} --num_samples=${num_samples} \

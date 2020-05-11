e50099c3fae67ee66a554ad46090f53f06735b46
Chase Duncan <cddunca2@frankenstein.bioen.illinois.edu>
2020-05-11 10:19:42 -0500

    Updated parameters of experiments.

--- run_experiments.sh
+++ run_experiments.sh
@@ -16,9 +16,9 @@ rank_param=0
 for num_samples in 1 5 10 20;do
   for num_users in 1 2 4 10;do
     exp_name="VC-${num_users}-${global_epochs}-${local_epochs}-${local_epochs_sampling}-${num_samples}-${rank_param}"
-    python main.py --exp_name=${exp_name} --num_users=${num_users} --global_epochs=${global_epochs} \
+    python main.py --exp_name=${exp_name} --num_users=${num_users} --global_epochs=${global_epochs} --threshold_test_metric=0.86\
       --local_epochs=${local_epochs} --local_epochs_sampling=${local_epochs_sampling} --num_samples=${num_samples} \
-      --fname="${exp_name}.npz" --rank_param=${rank_param} \
+      --fname="${exp_name}.npz" --rank_param=${rank_param} --train_batch_size=1024 --test_batch_size=1024 --global_lr=.2\
       --data_source=CIFAR10 --model=VGG16 --data_parallel --criterion=crossentropy   --dir="${dir}${exp_name}/"
   done
 done
@@ -28,9 +28,9 @@ for num_samples in 1 5 10 20;do
   for num_users in 2 4 10;do
     rank_param=$(($num_users-1))
     exp_name="VC-${num_users}-${global_epochs}-${local_epochs}-${local_epochs_sampling}-${num_samples}-${rank_param}"
-    python main.py --exp_name=${exp_name} --num_users=${num_users} --global_epochs=${global_epochs} \
+    python main.py --exp_name=${exp_name} --num_users=${num_users} --global_epochs=${global_epochs} --threshold_test_metric=0.86 \
       --local_epochs=${local_epochs} --local_epochs_sampling=${local_epochs_sampling} --num_samples=${num_samples} \
-      --fname="${exp_name}.npz" --rank_param=${rank_param} \
+      --fname="${exp_name}.npz" --rank_param=${rank_param} --train_batch_size=1024 --test_batch_size=1024 --global_lr=.2\
       --data_source=CIFAR10 --model=VGG16 --data_parallel --criterion=crossentropy   --dir="${dir}${exp_name}/"
   done
 done
